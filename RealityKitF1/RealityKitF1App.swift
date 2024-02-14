577e40b1b0770f9347dc75f65548bc45b8f20981
BlAcKPhOeNiX233 <bryansanchez.311@gmail.com>
2024-02-14 22:04:26 +0100

    Initial Commit

---
+++ RealityKitF1/RealityKitF1App.swift
@@ -0,0 +1,17 @@
+//
+//  RealityKitF1App.swift
+//  RealityKitF1
+//
+//  Created by Bryan Sánchez Peralta on 14/02/24.
+//
+
+import SwiftUI
+
+@main
+struct RealityKitF1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
